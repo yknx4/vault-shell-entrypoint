7a751315e8b8819520df33fc1ef40cfe24f02e6e
Ale Ornelas <jade.ornelas@homie.mx>
2022-01-27 08:47:15 -0600

    feat: create entry point

---
+++ vault-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+VAULT_SECRETS_DIR=${VAULT_SECRETS_DIR:='/vault/secrets'}
+echo "Attempting to load .env files from ${VAULT_SECRETS_DIR}."
+
+if [ -d "$VAULT_SECRETS_DIR" ]; then
+  FILES="${VAULT_SECRETS_DIR}/*.env"
+  for f in $FILES
+  do
+    echo "Processing $f file..."
+    # take action on each file. $f store current file name
+    source "$f"
+  done
+else
+  echo "$VAULT_SECRETS_DIR is not available. Skipping .env loading."
+fi
+
+$@
